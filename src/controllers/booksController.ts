2d49e076eee75a8934a157e3d519134fc96a0bf9
renato.cunha <renato.cunha@totvs.com.br>
2022-05-06 23:15:27 -0300

    add Atlas integration

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,16 +1,44 @@
 import books from '../data/books';
 import { Response, Request } from 'express';
+import { ObjectId } from 'mongodb';
+import { collections, connectToDatabase } from '../services/database.services';
+
 class BooksController {
-	static getAllBooks(req: Request, res: Response) {
-		return res.status(200).json({
-			books,
-			message: 'all books',
-		});
+
+	static async getBookById(req: Request, res: Response) {
+		const id = req?.params?.id;
+		await connectToDatabase();
+
+		try {
+			const query = { _id: new ObjectId(id) };
+			const book = await collections.books?.findOne(query);
+			if (book) {
+				res.status(200).send(book);
+			}
+		} catch (error) {
+			res
+				.status(404)
+				.send(
+					`unable to find any document`
+				);
+		}
 	}
 
-	static getBookById(req: Request, res: Response) {
-		const book = books.find(({ _id }) => _id === req.params.id) || {};
-		return res.status(200).json({book});
+	static async getAllBooks(req: Request, res: Response) {
+		await connectToDatabase();
+		try {
+			const book = await collections.books?.find().toArray();
+			console.log(book);
+			if (book) {
+				res.status(200).send(book);
+			}
+		} catch (error) {
+			res
+				.status(404)
+				.send(
+					`unable to find match matching document with id: ${req.params.id} `
+				);
+		}
 	}
 }
 export default BooksController;
---
+++ src/services/database.services.ts
@@ -0,0 +1,21 @@
+import * as mongoDB from 'mongodb';
+
+export const collections: { books?: mongoDB.Collection} = {}
+
+export async function connectToDatabase() {
+	const client: mongoDB.MongoClient = new mongoDB.MongoClient(
+		process.env.MONGODB_URI || ''
+	);
+
+	await client.connect();
+
+	const db: mongoDB.Db = client.db(process.env.DB_NAME || "socialbooks");
+
+	const booksCollection: mongoDB.Collection = db.collection('books');
+	
+	collections.books = booksCollection;
+
+	console.log(
+		`Successfully connected to database: ${db.databaseName} and collection: ${booksCollection.collectionName}`
+	);
+}
