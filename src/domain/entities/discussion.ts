1d512b3040f4596ee038b5448ce90e9a22942033
Giovana <giovanasachett@gmail.com>
2022-05-31 22:02:32 -0300

    add info to entities

--- src/domain/entities/discussion.ts
+++ src/domain/entities/discussion.ts
@@ -5,7 +5,11 @@ export default class Discussion {
         public registerDate: Date = new Date(), 
         public deactiveDate: Date | undefined, 
         public userId:number | undefined,
-        public bookId:number | undefined)
+        public bookId:number | undefined,
+        public userName: string | undefined,
+        public bookAuthor: string | undefined,
+        public bookShort: string | undefined,
+        public bookTitle: string | undefined)
         {}
 
 }
