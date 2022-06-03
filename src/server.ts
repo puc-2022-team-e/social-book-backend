cce367fe2b576aaf1d59b8559e4899b003e97222
renato.cunha <cunharenatodev@gmail.com>
2022-06-02 21:51:12 -0300

    remove comments

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,3 @@
-// import express from 'express';
-// import bodyParser from 'body-parser';
-// import routes from './routes/index';
-// import cors from 'cors';
-
-// const version = require('../package').version
-// const app = express();
-
-// const port = process.env.PORT || 8077;
-// const printServerMode = process.env.NODE_ENV || `development`
-// //kill container when Ctrl + C
-// process.on('SIGINT', function() {
-//     process.exit();
-// });
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// app.use('/api/v1/', routes);
-
-// app.listen(port, () => {
-// 	console.log(`Starting social books server
-// 	version: ${version}
-// 	in mode ${printServerMode}`)
-//     console.log(`Server started on port ${port}`);
-// });
-
-// //export for test
-// export default app;
-
 import config  from './config';
 import { DataBaseServices } from './services/database.services';
 import { HTTPServer } from './services/httpServer.services';
